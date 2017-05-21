2a3534f3a951190600f72024009c79a9b649a78c
MajinCry <cm@cmrms.com>
2017-05-21 17:42:08 +0100

    Initial Upload
    Custom classes are bugged

---
+++ Fallout 4 - SCLP From ASE/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Fallout_4___SCLP_From_ASE
+{
+    partial class windowProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupFilePaths = new System.Windows.Forms.GroupBox();
+            this.buttonSourcePath = new System.Windows.Forms.Button();
+            this.textboxSourcePath = new System.Windows.Forms.TextBox();
+            this.buttonModPath = new System.Windows.Forms.Button();
+            this.textboxModPath = new System.Windows.Forms.TextBox();
+            this.buttonMakeSCLP = new System.Windows.Forms.Button();
+            this.dialogSourcePath = new System.Windows.Forms.OpenFileDialog();
+            this.dialogModPath = new System.Windows.Forms.OpenFileDialog();
+            this.dialogMakeSCLP = new System.Windows.Forms.SaveFileDialog();
+            this.groupFilePaths.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupFilePaths
+            // 
+            this.groupFilePaths.Controls.Add(this.buttonMakeSCLP);
+            this.groupFilePaths.Controls.Add(this.textboxModPath);
+            this.groupFilePaths.Controls.Add(this.buttonModPath);
+            this.groupFilePaths.Controls.Add(this.textboxSourcePath);
+            this.groupFilePaths.Controls.Add(this.buttonSourcePath);
+            this.groupFilePaths.Location = new System.Drawing.Point(11, 14);
+            this.groupFilePaths.Name = "groupFilePaths";
+            this.groupFilePaths.Size = new System.Drawing.Size(532, 123);
+            this.groupFilePaths.TabIndex = 0;
+            this.groupFilePaths.TabStop = false;
+            this.groupFilePaths.Text = "ASE File Paths";
+            this.groupFilePaths.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // buttonSourcePath
+            // 
+            this.buttonSourcePath.Location = new System.Drawing.Point(6, 19);
+            this.buttonSourcePath.Name = "buttonSourcePath";
+            this.buttonSourcePath.Size = new System.Drawing.Size(75, 23);
+            this.buttonSourcePath.TabIndex = 0;
+            this.buttonSourcePath.Text = "Source";
+            this.buttonSourcePath.UseVisualStyleBackColor = true;
+            this.buttonSourcePath.Click += new System.EventHandler(this.buttonSourcePath_Click);
+            // 
+            // textboxSourcePath
+            // 
+            this.textboxSourcePath.Location = new System.Drawing.Point(87, 22);
+            this.textboxSourcePath.Name = "textboxSourcePath";
+            this.textboxSourcePath.Size = new System.Drawing.Size(430, 20);
+            this.textboxSourcePath.TabIndex = 1;
+            // 
+            // buttonModPath
+            // 
+            this.buttonModPath.Location = new System.Drawing.Point(6, 48);
+            this.buttonModPath.Name = "buttonModPath";
+            this.buttonModPath.Size = new System.Drawing.Size(75, 23);
+            this.buttonModPath.TabIndex = 2;
+            this.buttonModPath.Text = "Modified";
+            this.buttonModPath.UseVisualStyleBackColor = true;
+            this.buttonModPath.Click += new System.EventHandler(this.buttonModPath_Click);
+            // 
+            // textboxModPath
+            // 
+            this.textboxModPath.Location = new System.Drawing.Point(87, 50);
+            this.textboxModPath.Name = "textboxModPath";
+            this.textboxModPath.Size = new System.Drawing.Size(430, 20);
+            this.textboxModPath.TabIndex = 3;
+            // 
+            // buttonMakeSCLP
+            // 
+            this.buttonMakeSCLP.Enabled = false;
+            this.buttonMakeSCLP.Location = new System.Drawing.Point(226, 84);
+            this.buttonMakeSCLP.Name = "buttonMakeSCLP";
+            this.buttonMakeSCLP.Size = new System.Drawing.Size(75, 23);
+            this.buttonMakeSCLP.TabIndex = 4;
+            this.buttonMakeSCLP.Text = "Generate";
+            this.buttonMakeSCLP.UseVisualStyleBackColor = true;
+            this.buttonMakeSCLP.Click += new System.EventHandler(this.buttonMakeSCLP_Click);
+            // 
+            // dialogSourcePath
+            // 
+            this.dialogSourcePath.DefaultExt = "ASE";
+            this.dialogSourcePath.Filter = "3ds Max ASCII|*.ase";
+            // 
+            // dialogModPath
+            // 
+            this.dialogModPath.DefaultExt = "ASE";
+            this.dialogModPath.Filter = "3ds Max ASCII|*.ase";
+            // 
+            // dialogMakeSCLP
+            // 
+            this.dialogMakeSCLP.Filter = "Fallout 4 SCLP|*.sclp";
+            // 
+            // windowProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 140);
+            this.Controls.Add(this.groupFilePaths);
+            this.Name = "windowProgram";
+            this.Text = "Fallout 4 - SCLP From ASE";
+            this.groupFilePaths.ResumeLayout(false);
+            this.groupFilePaths.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupFilePaths;
+        private System.Windows.Forms.TextBox textboxModPath;
+        private System.Windows.Forms.Button buttonModPath;
+        private System.Windows.Forms.TextBox textboxSourcePath;
+        private System.Windows.Forms.Button buttonSourcePath;
+        private System.Windows.Forms.Button buttonMakeSCLP;
+        private System.Windows.Forms.OpenFileDialog dialogSourcePath;
+        private System.Windows.Forms.OpenFileDialog dialogModPath;
+        private System.Windows.Forms.SaveFileDialog dialogMakeSCLP;
+    }
+}
+
---
+++ Fallout 4 - SCLP From ASE/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Fallout_4___SCLP_From_ASE
+{
+    public partial class windowProgram : Form
+    {
+        //Begin variables setup
+        public class classASEBone
+        {
+            string _NodeName = new string("Null".ToCharArray());
+            classASENodeTM _ASENodeTM = new classASENodeTM();
+
+            public string NodeName
+            {
+                get { return _NodeName; }
+                set { _NodeName = value; }
+            }
+
+            public classASENodeTM ASENodeTM
+            {
+                get { return _ASENodeTM; }
+                set { _ASENodeTM = value; }
+            }
+
+        }
+
+        public class classASENodeTM
+        {
+            string _strTMScaleX = new string("Null".ToCharArray());
+            string _strTMScaleY = new string("Null".ToCharArray());
+            string _strTMScaleZ = new string("Null".ToCharArray());
+
+            public string strTMScaleX
+            {
+                get { return _strTMScaleX; }
+                set { _strTMScaleX = value; }
+            }
+
+            public string strTMScaleY
+            {
+                get { return _strTMScaleY; }
+                set { _strTMScaleY = value; }
+            }
+
+            public string strTMScaleZ
+            {
+                get { return _strTMScaleZ; }
+                set { _strTMScaleZ = value; }
+            }
+
+        }
+
+        public string strSourcePath;
+        public string strModPath;
+        public static string strFindGeomObject = "*GEOMOBJECT {";
+        public static string strFindNodeName = "\t" + "*NODE_NAME " + "\"";
+        public static string strFindTMScale = "\t" + "\t" + "*TM_SCALE ";
+
+        public int iFindTMScaleLength = strFindTMScale.Length;
+        public int iFindNodeNameLength = strFindNodeName.Length;
+        //End
+
+        #region
+        public windowProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSourcePath_Click(object sender, EventArgs e)
+        {
+
+            if (dialogSourcePath.ShowDialog() == DialogResult.OK)
+            {
+                strSourcePath = dialogSourcePath.FileName;
+                textboxSourcePath.Text = strSourcePath;
+
+                if (File.Exists(textboxModPath.Text))
+                {
+                    buttonMakeSCLP.Enabled = true;
+                }
+
+            }
+
+        }
+
+        private void buttonModPath_Click(object sender, EventArgs e)
+        {
+
+            if (dialogModPath.ShowDialog() == DialogResult.OK)
+            {
+                strModPath = dialogModPath.FileName;
+                textboxModPath.Text = strModPath;
+
+                if (File.Exists(textboxSourcePath.Text))
+                {
+                    buttonMakeSCLP.Enabled = true;
+                }
+
+            }
+
+        }
+        #endregion
+
+
+        private string[] GetScalingXYZ(string strLine)
+        {
+            string[] arraystrXYZ = new string[3];
+            string strLineMod = strLine;
+            string strCurrentValue;
+
+            int iTabIndex;
+
+
+
+            strLineMod = strLineMod.Substring(iFindTMScaleLength, (strLineMod.Length - iFindTMScaleLength));
+            iTabIndex = strLineMod.IndexOf("\t");
+            strCurrentValue = strLineMod.Substring(0, iTabIndex);
+            arraystrXYZ[0] = strCurrentValue;
+
+            strLineMod = strLineMod.Substring(iTabIndex + 1, strLineMod.Length - (iTabIndex + 1));
+            iTabIndex = strLineMod.IndexOf("\t");
+            strCurrentValue = strLineMod.Substring(0, iTabIndex);
+            arraystrXYZ[1] = strCurrentValue;
+
+            strLineMod = strLineMod.Substring(iTabIndex + 1, strLineMod.Length - (iTabIndex + 1));
+            strCurrentValue = strLineMod.Substring(0, strLineMod.Length);
+            arraystrXYZ[2] = strCurrentValue;
+
+
+            return arraystrXYZ;
+        }
+
+
+        private string GetBoneName(string strLine)
+        {
+            string strBoneName;
+            string strLineMod = strLine;
+            int iQuotationIndex;
+
+            strLineMod = strLineMod.Substring(iFindNodeNameLength, strLineMod.Length - iFindNodeNameLength);
+            iQuotationIndex = strLineMod.IndexOf("\"");
+            strBoneName = strLineMod.Substring(0, iQuotationIndex);
+
+            return strBoneName;
+        }
+
+
+        //Begin Main
+        private void buttonMakeSCLP_Click(object sender, EventArgs e)
+        {
+
+            #region
+            List<string> listSourceASE = File.ReadLines(strSourcePath).ToList();
+            List<string> listModASE = File.ReadLines(strModPath).ToList();
+
+            List<int> listiGeomObjectIndex = new List<int>();
+            List<int> listiNodeNameIndex = new List<int>();
+            List<int> listiNodeTMIndex = new List<int>();
+            List<int> listiTMScaleIndex = new List<int>();
+            List<int> listiDuplicateBoneIndex = new List<int>();
+
+            int iNumBones = 0;
+            #endregion
+
+            int iCounter;
+
+            //Get the indexes of the bone structures' data
+            #region
+            for (iCounter = 0; iCounter <= (listSourceASE.Count - 1); iCounter++)
+            {
+                if (listSourceASE[iCounter].Contains(strFindGeomObject))
+                {
+                    listiGeomObjectIndex.Add(iCounter + 1);
+                    listiNodeNameIndex.Add(iCounter + 2);
+                    listiNodeTMIndex.Add(iCounter + 3);
+                    listiTMScaleIndex.Add(iCounter + 15);
+                    iNumBones++;
+                }
+            }
+
+
+            //Get indexes of unchanged bones
+            for (iCounter = 0; iCounter <= (iNumBones - 1); iCounter++)
+            {
+                if (listSourceASE[listiTMScaleIndex[iCounter]] == listModASE[listiTMScaleIndex[iCounter]])
+                {
+                    listiDuplicateBoneIndex.Add(listiGeomObjectIndex[iCounter]);
+                }
+            }
+
+            //Remove unchanged bone indexes from lists
+            foreach (int iIndex in listiDuplicateBoneIndex)
+            {
+                
+                for (iCounter = (iNumBones - 1); iCounter >= 0; iCounter--)
+                {
+                    if (iIndex == listiGeomObjectIndex[iCounter])
+                    {
+                        listiGeomObjectIndex.RemoveAt(iCounter);
+                        listiNodeNameIndex.RemoveAt(iCounter);
+                        listiNodeTMIndex.RemoveAt(iCounter);
+                        listiTMScaleIndex.RemoveAt(iCounter);
+                        iNumBones--;
+                    }
+                }
+
+            }
+            #endregion
+
+
+
+            //Create arrays for all the modified bones
+            classASEBone[] arrayaseboneMod = new classASEBone[iNumBones];
+            string[] arraystrBoneXYZ = new string[3];
+            string strBoneName;
+
+            string strLineTMScale;
+            string strLineNodeName;
+
+            for (iCounter = 0; iCounter <= (iNumBones - 1); iCounter++)
+            {
+                classASEBone aseboneTemp = arrayaseboneMod[iCounter];
+
+                classASENodeTM asenodeTemp = new classASENodeTM();
+
+
+                //Get Bone name
+                strLineNodeName = listModASE[listiNodeNameIndex[iCounter]];
+                strBoneName = GetBoneName(strLineNodeName);
+                //end
+
+                strLineTMScale = listModASE[listiTMScaleIndex[iCounter]];
+                arraystrBoneXYZ = GetScalingXYZ(strLineTMScale);
+                
+
+                //Create the bone data
+                aseboneTemp.NodeName = strBoneName;
+
+                asenodeTemp.strTMScaleX = arraystrBoneXYZ[0];
+                asenodeTemp.strTMScaleY = arraystrBoneXYZ[1];
+                asenodeTemp.strTMScaleZ = arraystrBoneXYZ[2];
+
+                aseboneTemp.ASENodeTM = asenodeTemp;
+                //end
+
+
+                //Bone data is finished, now we add it to the array
+                arrayaseboneMod[iCounter] = aseboneTemp;
+                //Et voila
+            }
+
+
+
+        }
+        //End
+
+
+    }
+
+}
